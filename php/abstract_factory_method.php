491e758263685fe4b5f238c3866ec11157e4bf1d
Gabriel Fonseca <fonsecacrz7@gmail.com>
2020-10-03 17:36:58 -0300

    added abstract factory method

--- php/abstract_factory_method.php
+++ php/abstract_factory_method.php
@@ -1,2 +1,102 @@
 <?php
 
+interface Product {
+    public function show(): string;
+    public function color(): string;
+}
+
+class ProductGreenOne implements Product {
+	public function show(): string {
+		return "product green 1";
+    }
+    
+    public function color(): string {
+        return "i am green";
+    }
+}
+
+class ProductBlueOne implements Product {
+	public function show(): string {
+		return "product blue 1";
+    }
+    
+    public function color(): string {
+        return "i am blue";
+    }
+}
+
+class ProductGreenTwo implements Product {
+	public function show(): string {
+		return "product green 2";
+    }
+    
+    public function color(): string {
+        return "i am green";
+    }
+}
+
+class ProductBlueTwo implements Product {
+	public function show(): string {
+		return "product blue 2";
+    }
+    
+    public function color(): string {
+        return "i am blue";
+    }
+}
+
+interface Creator {
+	public function createProductBlue(): Product;
+	public function createProductGreen(): Product;
+}
+
+class ConcreteProductCreatorOne implements Creator {
+	public function createProductGreen(): ProductGreenOne {
+        return new ProductGreenOne();
+    }
+
+    public function createProductBlue(): ProductBlueOne {
+        return new ProductBlueOne();
+    }
+}
+
+class ConcreteProductCreatorTwo implements Creator {
+	public function createProductGreen(): ProductGreenTwo {
+        return new ProductGreenTwo();
+    }
+
+    public function createProductBlue(): ProductBlueTwo {
+        return new ProductBlueTwo();
+    }
+}
+
+interface CreatorColor {
+    public function createProductOne(): Product;
+    public function createProductTwo(): Product;
+}
+
+class ConcreteProductCreatorGreen implements CreatorColor {
+    public function createProductOne(): ProductGreenOne {
+        return new ProductGreenOne();
+    }
+
+    public function createProductTwo(): ProductGreenTwo {
+        return new ProductGreenTwo();
+    }
+}
+
+function client(Creator $creator): void {
+    $product = $creator->createProductBlue();
+
+    echo "{$product->show()} - {$product->color()}\n";
+}
+
+function clientColor(CreatorColor $creator): void {
+    $product = $creator->createProductTwo();
+
+    echo "{$product->show()} - {$product->color()}\n";
+}
+
+client(new ConcreteProductCreatorOne());
+
+clientColor(new ConcreteProductCreatorGreen());
--- python/abstract_factory_method.py
+++ python/abstract_factory_method.py
@@ -1,2 +1,71 @@
 #!/usr/bin/env python3
 
+from abc import ABC, abstractmethod
+
+
+class Button(ABC):
+    @abstractmethod
+    def create(self) -> str:
+        pass
+
+
+class ButtonLargeWindows(Button):
+    def create(self) -> str:
+        return "large button windows"
+
+
+class ButtonLargeLinux(Button):
+    def create(self) -> str:
+        return "large button linux"
+
+
+class ButtonShortWindows(Button):
+    def create(self) -> str:
+        return "short button windows"
+
+
+class ButtonShortLinux(Button):
+    def create(self) -> str:
+        return "short button linux"
+
+
+class ButtonCreator(ABC):
+    @abstractmethod
+    def createWindowsButton(self) -> Button:
+        pass
+
+    @abstractmethod
+    def createLinuxButton(self) -> Button:
+        pass
+
+
+class LargeButtonCreator(ButtonCreator):
+    def createWindowsButton(self) -> ButtonLargeWindows:
+        return ButtonLargeWindows()
+
+    def createLinuxButton(self) -> ButtonLargeLinux:
+        return ButtonLargeLinux()
+
+
+class ShortButtonCreator(ButtonCreator):
+    def createLinuxButton(self) -> ButtonShortLinux:
+        return ButtonShortLinux()
+
+    def createWindowsButton(self) -> ButtonShortWindows:
+        return ButtonShortWindows()
+
+
+def largeClient(creator: ButtonCreator):
+    button = creator.createWindowsButton()
+
+    print(f"{button.create()}")
+
+
+def shortClient(creator: ButtonCreator):
+    button = creator.createLinuxButton()
+
+    print(f"{button.create()}")
+
+
+largeClient(LargeButtonCreator())
+shortClient(ShortButtonCreator())
